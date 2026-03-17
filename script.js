b916fa93e427f5d3b0abf48927de18509e29a763
Aluno <aluno@fatecmm.local>
2026-03-17 15:00:24 -0300

    Estilização geral, adição de vocabulario e tempos verbais pode ser feita sem problemas

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Alterado de vocab-div para vocabDiv
 const vocabDiv = document.getElementById("vocab");
 const tempoDiv = document.getElementById("tempo");
+const toggleBtn = document.getElementById("toggle");
 
 const vocabList = [
     { palavra: "見る", godan: false },
@@ -25,37 +26,100 @@ const vocabList = [
     { palavra: "呼ぶ", godan: true }
 ];
 
-const conjugList = ["Passado", "Te-Form"];
+const tempoList = [
+    "Passado",
+     "Passado Educado",
+      "Te-Form",
+     "Te-form Educado"
+    ];
 
-function toggleConteudo() {
-    vocabDiv.classList.toggle("aberto");
+function toggleConteudo(num) {
+    switch (num) {
+        case 1:
+            vocabDiv.classList.toggle("aberto");
+            break;
+    
+        case 2:
+            tempoDiv.classList.toggle("aberto");
+            break;
+    }
+    
 }
 
-function loadConteudo() {
+function loadConteudo() {   
+    // -- Carregar os vocabularios
+
+    //Cria o ul default
     const lista = document.createElement("ul");
-    
-    // Usamos 'let' porque o 'i' vai aumentar de valor
     for (let i = 0; i < vocabList.length; i++) {
+        //Cria o elemento li e guarda a palavra nele
         const addVocab = document.createElement("li");
         addVocab.textContent = vocabList[i].palavra;
         
-        // Corrigido de list para lista
+        //Guarda o elemento no ul
         lista.appendChild(addVocab);
     }
+    //Guarda o elemento ul no div correspondente
     vocabDiv.appendChild(lista);
     
-    for (let i = 0; i < tempoDiv.length; i++) {
+    // -- Carregar o tempo dos verbos
+
+    //Cria o elemento ul intermediario
+    const listaTempo = document.createElement("ul");
+    for (let i = 0; i < tempoList.length; i++) {
+        //Cria o li correspondente
+        const AddLi = document.createElement("li");
+
+        //Cria o elemento input e configura ele
         const addTempo = document.createElement("input");
-        addTempo.setAtribute("type", "radio");
-        addTempo.setAtribute("id", i);
-        
+        addTempo.setAttribute("type", "checkbox");
+        //Define a regra do nome dele
+        let nome = "tempo-" + (i+1); 
+        addTempo.setAttribute("id", nome);
+        addTempo.setAttribute("name", nome);
+        addTempo.setAttribute("value", tempoList[i]);
+
+        //Cria o elemento label e configura ele
         const addLabel = document.createElement("label");
-        addLabel.setAttribute("for", i);
+        addLabel.setAttribute("for", nome);
         addLabel.textContent = tempoList[i];
-        // Corrigido de list para lista
-        tempoDiv.appendChild(addTempo);
-        tempoDiv.appendChild(addLabel);
+        
+        //Guarda os elementos dentro do Li
+        AddLi.appendChild(addTempo);
+        AddLi.appendChild(addLabel);
+
+        //Colocando o Li dentro do Ul
+        listaTempo.appendChild(AddLi);
     }
+    tempoDiv.appendChild(listaTempo);
+}
+
+function toggleOn(){
+    toggleBtn.classList.toggle("toggled");
+
+    const elementos = tempoDiv.querySelectorAll('*');
+
+    for(let i = 0; i < elementos.length; i++){
+        if(elementos[i].nodeName = "INPUT"){
+            elementos[i].checked = !elementos[i].checked;
+            console.log("Achado")
+        }
+    }
+    console.log("Setado")
+}
+
+function untoggleOn(){
+    toggleBtn.classList.toggle("toggled");
+
+    const elementos = tempoDiv.querySelectorAll('*');
+
+    for(let i = 0; i < elementos.length; i++){
+        if(elementos[i].nodeName = "INPUT"){
+            elementos[i].checked = false    ;
+            console.log("Achado")
+        }
+    }
+    console.log("Setado")
 }
 
 loadConteudo();
